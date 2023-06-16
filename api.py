d5266d51aff8d99f49ff6aa1081c9b9bc2e64769
Taoufik Assabar <86358255+Assabar70@users.noreply.github.com>
2023-06-16 15:39:29 +0100

    Update api.py

--- api.py
+++ api.py
@@ -1,17 +1,17 @@
 import numpy as np
 import tensorflow as tf
 import librosa
-import json 
-from flask import Flask, request, jsonify
+import json
 from pymongo import MongoClient
+from flask import Flask
 
 app = Flask(__name__)
 
-# Configuration de la connexion à MongoDB Atlas
-username_db = "e-stetho"
-password_db = "e-stetho"
-mongodb_url = f"mongodb+srv://{username_db}:{password_db}@iot-cluster.omkaadn.mongodb.net/?retryWrites=true&w=majority"
-database_name = 'sound_samples'
+# MongoDB connection details
+#username_db = "e-stetho"
+#password_db = "e-stetho"
+mongodb_url ="mongodb+srv://Daoud:BAPft9yNGn9PGrmY@stetho.hyltfoz.mongodb.net/Stetho?retryWrites=true&w=majority"
+database_name = 'Stetho'
 collection_name = 'sound'
 
 # Connect to MongoDB
@@ -22,19 +22,12 @@ collection = db[collection_name]
 # Charger votre modèle TensorFlow ici
 model = tf.keras.models.load_model('my_model.h5')
 
-@app.route('/detect-anomaly', methods=['GET'])
-def detect_anomaly():
-    # Récupérer les données audio depuis MongoDB Atlas
-    data = collection.find()
-    samples = []
-    for d in data:
-        samples += d["samples"]
-    audio_data = np.array(samples)
-    
+
+def detect_anomaly(audio_data):
     # Convertir les données audio en nombres flottants
     audio_data_float = np.array(audio_data, dtype=np.float32) / np.iinfo(np.int16).max
 
-   # Fixer la taille des données audio à 3.5 secondes (en fonction du taux d'échantillonnage)
+    # Fixer la taille des données audio à 3.5 secondes (en fonction du taux d'échantillonnage)
     desired_duration = 3.5  # Durée souhaitée en secondes
     sample_rate = 22050  # Taux d'échantillonnage
     desired_length = int(desired_duration * sample_rate)
@@ -42,7 +35,7 @@ def detect_anomaly():
     # Vérifier si les données audio doivent être tronquées ou étendues
     if len(audio_data_float) < desired_length:
         if len(audio_data_float) < sample_rate * 3.5:
-            return jsonify({'error': 'La durée de l\'audio est inférieure à 3.5 secondes.'}), 400
+            return {'error': 'La durée de l\'audio est inférieure à 3.5 secondes.'}
         # Étendre les données audio en ajoutant des zéros à la fin
         audio_data_fixed = np.pad(audio_data_float, (0, desired_length - len(audio_data_float)), mode='constant')
     elif len(audio_data_float) > desired_length:
@@ -65,12 +58,36 @@ def detect_anomaly():
 
     # Définir la liste des noms de classes d'anomalies
     classes = ["Artifact", "Extrahls", "Extrastole", "Murmur", "Normal"]
-   
-    # Retourner la réponse sous forme de JSON
-    return jsonify({
+
+    return {
         'anomalie_detectee': classes[anomalie_detectee],
         'pourcentage_probabilite': pourcentage_probabilite
-    }), 200
+    }
+
+
+@app.route('/')
+def process_audio_data():
+    # Récupérer les données audio depuis MongoDB Atlas
+    data = collection.find()
+    for d in data:
+        audio_data = d["samples"]
+        result = detect_anomaly(audio_data)
+        anomalie_detectee = result['anomalie_detectee']
+        pourcentage_probabilite = result['pourcentage_probabilite']
+
+        # Mettre à jour la collection MongoDB avec les résultats de prédiction
+        collection.update_one(
+            {'_id': d['_id']},  # Filtrer le document par ID
+            {
+                "$set": {
+                    "anomalie_detectee": anomalie_detectee,
+                    "pourcentage_probabilite": pourcentage_probabilite
+                }
+            }
+        )
+        print("Résultats de prédiction ajoutés à la collection avec succès.")
+
+    return "Toutes les données ont été traitées."
 
 
 if __name__ == '__main__':
